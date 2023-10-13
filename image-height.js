f9d475bdbb3806de71d9d74dadb2048c670b6eae
dapeef <42572235+dapeef@users.noreply.github.com>
2023-10-13 12:03:09 +0100

    Fix profile pic resizing

--- image-height.js
+++ image-height.js
@@ -2,24 +2,32 @@ var containerElement = document.getElementById("welcome_container");
 var textElement = document.getElementById("welcome_text");
 var imageElement = document.getElementById("profile_photo");
 
-//Cancel approximation when the height difference is less than DELTA.
-var DELTA = 1;
+var DELTA = .25; // Cancel approximation when the height difference is less than DELTA.
+var border_width = 3; // Thickness of the image's border
 
 var adjustImageHeight = function() {
     console.log(imageElement.offsetHeight);
     console.log(textElement.offsetHeight);
 
-    while(Math.abs(textElement.offsetHeight - imageElement.offsetHeight) > DELTA) {
-        imageElement.style.height = ((imageElement.offsetHeight + textElement.offsetHeight) / 2) + 'px';   
-        console.log(imageElement.offsetHeight) 
+    var counter = 0;
+
+    while(Math.abs(textElement.offsetHeight - (imageElement.offsetHeight)) > DELTA && counter < 100)  {
+        console.log(imageElement.offsetHeight + " " + textElement.offsetHeight)
+        imageElement.style.height = ((imageElement.offsetHeight + textElement.offsetHeight) / 2 - border_width*2) + 'px';   
+        console.log(imageElement.offsetHeight)
         
         //Image grows larger than container, reset its size.
         if(imageElement.offsetWidth > containerElement.offsetWidth) {
             imageElement.style.height = 'auto';
             return;
         };
+
+        counter ++;
     };
 };
 
-// window.addEventListener('resize', adjustImageHeight);
-// adjustImageHeight();
+window.addEventListener('resize', adjustImageHeight);
+adjustImageHeight();
+// imageElement.style.height = "22em"
+
+// https://jsbin.com/puyuqehube/1/edit?html,css,js,output
